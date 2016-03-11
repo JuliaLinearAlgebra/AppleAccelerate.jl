cdf0916dc8fbdf0bfd27f1e3520f00e189f37e89
Remy Prechelt <rprechelt@gmail.com>
2016-03-11 11:57:58 -0600

    Refactored package files (Issue #13)
    Moved Array functions into Array.jl, DSP functions into DSP.jl, and @replaceBase into Util.jl.

---
+++ src/Util.jl
@@ -0,0 +1,40 @@
+## Util.jl##
+
+if VERSION>=v"0.4.0-rc1"
+    tupletypelength(a)=length(a.parameters)
+else
+    tupletypelength(a)=length(a)
+end
+
+
+macro replaceBase(fs...)
+    b = Expr(:block)
+    for f in fs
+        if f == :./
+            fa = :fdiv
+        elseif f == :.^
+            fa = :pow
+        else
+            fa = f
+        end
+        if fa in (:ceil,:floor,:sqrt,:rsqrt,:rec,
+                  :exp,:exp2,:expm1,:log,:log1p,:log2,:log10,
+                  :sin,:sinpi,:cos,:cospi,:tan,:tanpi,:asin,:acos,:atan,
+                  :sinh,:cosh,:tanh,:asinh,:acosh,:atanh,
+                  :trunc,:round,:exponent,:abs,:sincos,:cis)
+            e = quote
+                (Base.$f)(X::Array{Float64}) = ($fa)(X)
+                (Base.$f)(X::Array{Float32}) = ($fa)(X)
+            end
+        elseif fa in (:copysign,:atan2,:pow,:rem,:fdiv)
+            e = quote
+                (Base.$f)(X::Array{Float64},Y::Array{Float64}) = ($fa)(X,Y)
+                (Base.$f)(X::Array{Float32},Y::Array{Float32}) = ($fa)(X,Y)
+            end
+        else
+            error("Function $f not defined by Accelerate")
+        end
+        push!(b.args,e)
+    end
+    b
+end
